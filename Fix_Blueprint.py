b224d1e4d3aecef8077f0e4544edae5153df5bdb
Mahmoud Aziz <41173335+mahmoudadelaziz@users.noreply.github.com>
2022-09-26 23:58:50 +0200

    Add files via upload
    Uploaded the first source code file.

---
+++ Fix_Blueprint.py
@@ -0,0 +1,8 @@
+"""
+This code should serve as a blueprint to do the following for each "batch"*:
+---> "Create" the objects corresponding to the datapoints.
+---> "Send" the datapoints.
+---> 
+
+* The number of elements in each batch is still to be determined.
+"""
